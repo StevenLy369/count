c77acdf63ce01b109b1cce4da3296b1b8228c485
Steven Ly <1stevenly92@gmail.com>
2020-03-31 09:07:35 -0700

    added basic html and script files

---
+++ js/script.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+    var countTo;
+    var countBy;
+
+    
+    $("#button").click(function(button){
+        event.preventDefault();
+        for (var index = 1; index <=countTo; index ++ ) {
+
+            
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+    
+
+
+
+
+
+
+
+
+})
